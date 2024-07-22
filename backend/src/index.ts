d284f6f02138e7f75e5af023f293599c6745d880
nithin6x9 <nithinkrishnalal303@gmail.com>
2024-07-22 20:57:05 +0530

    x

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,14 +3,31 @@ import {PrismaClient} from "@prisma/client/edge";
 import {withAccelerate} from "@prisma/extension-accelerate";
 
 
-const app = new Hono();
+const app = new Hono<{
+	Bindings:{
+		DATABASE_URL: string
+	}
+}>();
 
 app.post("api/v1/signup",(c)=>{
+	//const dburl = c.env.DATABASE_URL
 	const prisma = new PrismaClient({
-		datasourceUrl: env.DATABASE_URL,
+		datasourceUrl: c.env.DATABASE_URL,
 	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+
+	await prisma.user.create({
+		data:{
+			email:c.body.email,
+			password:c.body.password,
+		},
+	});
+
+
 	return c.text("Hello Hono");
 });
+
 app.post("api/v1/signin",(c)=>{
 	return c.text("Hello Hono");
 });
