7fc8457a49fa1e43410a8d754e82e2dd07a37222
nithin6x9 <nithinkrishnalal303@gmail.com>
2024-07-23 18:25:18 +0530

    x

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,28 @@
 import { Hono } from "hono";
-import {sign} from "hono/jwt";
+import {sign,verify} from "hono/jwt";
 import {PrismaClient} from "@prisma/client/edge";
 import {withAccelerate} from "@prisma/extension-accelerate";
 
 
 const app = new Hono();
+
+app.use("/api/v1/blog/*",async(c,next) => {
+
+	//get the header
+	//verify the header
+	//if the header is correct ,we can process
+	//if not we return the user a 403 status code
+	const header = c.req.header("authorization") || "";
+
+	const response = await verify(header,c.env.JWT_SECRET); 
+	if(reponse.id){
+		next();
+	} else{
+
+		c.status(403);
+		await next();
+	}
+});
 
 app.post("/api/v1/signup", async (c) => {
 	const prisma = new PrismaClient({
@@ -26,7 +44,7 @@ app.post("/api/v1/signup", async (c) => {
 	});
 });
 
-app.post("/api/v1/signin",async (c)=>{
+app.post("/api/v1/signin",async (c) => {
 
 	const prisma = new PrismaClient({
 		datasourceUrl:c.env?.DATABASE_URL,
