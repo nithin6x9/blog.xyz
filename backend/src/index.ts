86c9a38c16fd03765c6c129ed5e8f233022dc53f
nithin6x9 <nithinkrishnalal303@gmail.com>
2024-07-24 19:50:13 +0530

    signin end point

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -36,5 +36,37 @@ app.post("/api/v1/signup", async (c) => {
 		return c.text("USer already exist");
 	}
 });
+app.post("/api/v1/signin", async(c) => {
+	const body = await c.req.json();
+	const prisma = new PrismaClient({
+		datasourceUrl:c.env?.DATABASE_URL,
+	}).$extends(withAccelerate());
+
+	try{
+		const user = await prisma.user.findFirst({
+			where:{
+				email:body.email,
+				password:body.password
+			}
+		});
+		if(!user){
+			c.status(403);//403 unauthorized status code
+			//return c.text("Invalid credentials");
+			return c.json({
+				message:"Incorrect credentials"
+			});
+		}
+		const jwt = await sign({
+			id:user.id
+		},c.env.JWT_SECRET);
+
+		return c.text(jwt);
+	}catch(e){
+		console.log(e);
+		c.status(411);
+		return c.text("Invalid");
+
+	}
+});
 
 export default app;
