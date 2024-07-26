48e4caa526fe0c77efc151dd2e00af76616619c3
nithin6x9 <nithinkrishnalal303@gmail.com>
2024-07-26 12:57:36 +0530

    npm publish

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,6 +2,7 @@ import {Hono} from "hono";
 import {sign} from "hono/jwt";
 import {PrismaClient} from "@prisma/client/edge";
 import {withAccelerate} from "@prisma/extension-accelerate";
+import {signupInput} from "@nithin0.9/medium-clone";
 
 
 export const userRouter = new Hono<{
--- common/src/index.ts
+++ common/src/index.ts
@@ -1,12 +1,12 @@
 import {z} from "zod";
 
-export const singupInput = z.object({
+export const signupInput = z.object({
 	email:z.string().email(),
 	password:z.string().min(6),
 	name:z.string().optional()
 });
 
-export const singinInput = z.object({
+export const signinInput = z.object({
 	email:z.string().email(),
 	password:z.string().min(6),
 	name:z.string().optional()
