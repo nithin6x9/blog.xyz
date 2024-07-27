88bb5aff6d24142ec68e833a82d9624c12f10a5b
nithin6x9 <nithinkrishnalal303@gmail.com>
2024-07-27 14:09:27 +0530

    init

---
+++ frontend/src/components/quote.tsx
@@ -0,0 +1,10 @@
+
+
+
+export const Quote = ()=>{
+	return <div className="bg-slate-200 h-screen flex justify-center flex-col"> 
+		<div>
+			Hi from quote component
+		</div>
+	</div>
+}
---
+++ frontend/src/pages/Signin.tsx
@@ -0,0 +1,5 @@
+export const Signin = () => {
+	return <div>
+			<h1> This is sign In page </h1>
+		</div>
+}
