25fee6fd0521a9f8f1aee1a15dbd4449f55a2f77
nithin6x9 <nithinkrishnalal303@gmail.com>
2024-07-28 19:58:17 +0530

    auth ui init

--- frontend/src/components/auth.tsx
+++ frontend/src/components/auth.tsx
@@ -1,13 +1,50 @@
-export const Auth = ({type}:{type:"signup || singin"})=>{
+import {Link} from "react-router-dom";
+import {ChangeEvent,useState} from 'react';
+import {SignupInput} from "@nithin0.9/medium-clone";
+
+export const Auth = ({type}:{type:"signup" | "singin"})=>{
+	const [postInputs,setPostInputs] = useState<SignupInput>({
+		name:"",
+		email:"",
+		password:""
+	});
+
 	return (
-		<div className = "bg-slate-100  h-screen flex justify-center flex-col">
-			<div className="flex justify-center">
-				<div className = "text-xl font-semibold text-left mt-4">
-					Create an Account
+		<div className = "bg-slate-100 h-screen flex justify-center flex-col">
+			{JSON.stringify(postInputs)}
+			<div className = "flex justify-center" >
+				<div>
+					<div className="text-3xl font-extrabold">
+						Create an Account
+					</div>
+					<div>
+						Already have an acoount? <Link className="pl-1 underline" to={"/signin"} >Login </Link>
+					</div>
 				</div>
+				<LabelledInput title="Name" placeholder="Nithin Krishna" onChange={(e)=>{
+					setPostInputs(c => ({
+						...c,
+						name:e.target.value
+					}))
+				}}/>
+
+
 			</div>
 		</div>
 	)
 }
+interface LabelledInputType{
+	label:string;
+	placeholder:string;
+	onChange:(e:ChangeEvent<HTMLInputElement>)=>void;
 
+}
+
+function LabelledInput({label,placeholder,onChange}:LabelledInputType){
+	return <div>
+	<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
+	<input onChange = {onChange} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} required />
+</div>
+
+}
 
