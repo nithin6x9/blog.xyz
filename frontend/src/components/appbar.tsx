5a5cae06fdec1a8e184fd89d9fce3572559899f0
nithin6x9 <nithinkrishnalal303@gmail.com>
2024-08-01 19:29:48 +0530

    buggie

--- frontend/src/components/appbar.tsx
+++ frontend/src/components/appbar.tsx
@@ -3,15 +3,17 @@ import {Link} from "react-router-dom";
 
 
 export const Appbar = ()=>{
-	return <div className = "border-b flex justify-between px-10 py-4">
-		<Link to={'/blogs'} className="flex flex-col justify-center">
-			Blog.xyz
-		</Link>
-		<div>
-			<Link to = {`/publish`}>
-			<button type="button" className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-4 me-2 mb-2">New blog</button>
-			</Link>
-			<Avatar size = {"big"} name="Nithin"/>
-		</div>
-		</div>
+	return (
+			<div className = "border-b flex justify-between px-10 py-4">
+				<Link to={'/blogs'} className="flex flex-col justify-center">
+					Blog.xyz
+				</Link>
+				<div>
+					<Link to = {`/publish`}>
+					<button type="button" className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-4 me-2 mb-2">New blog</button>
+					</Link>
+					<Avatar size = {"big"} name="Nithin"/>
+				</div>
+				</div>
+	)
 }
