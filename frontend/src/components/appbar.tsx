1aea977a412aebcd6c831805a62d7b58b99ebfdf
nithin6x9 <nithinkrishnalal303@gmail.com>
2024-08-01 15:47:52 +0530

    minor updates

--- frontend/src/components/appbar.tsx
+++ frontend/src/components/appbar.tsx
@@ -1,12 +1,14 @@
 import {Avatar} from "./blogCard";
+import {Link} from "react-router-dom";
+
 
 export const Appbar = ()=>{
 	return <div className = "border-b flex justify-between px-10 py-4">
-		<div className="flex flex-col justify-center">
-			Medium
-		</div>
+		<Link to={'/blogs'} className="flex flex-col justify-center">
+			Blog.xyz
+		</Link>
 		<div>
-			<Avatar size = {6} name="Nithin"/>
+			<Avatar size = {"big"} name="Nithin"/>
 		</div>
 		</div>
 }
--- frontend/src/components/fullblog.tsx
+++ frontend/src/components/fullblog.tsx
@@ -1,21 +1,34 @@
 import {Appbar} from './appbar';
 import {Blog} from '../hooks/index.ts';
-
+import {Avatar} from "../components/blogCard";
 
 export const FullBlog = ({ blog }: {blog: Blog}) => {
 	return <div>
 			<Appbar />
-				<div className = "grid grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12">
-					<div className = "col-span-8">
-						<div className = "text-3xl font-extrabold">
-							{blog.title}
+				<div className = "flex justify-center">
+					<div className = "grid grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12">
+						<div className = "col-span-8">
+							<div className = "text-3xl font-extrabold">
+								{blog.title}
+							</div>
+							<div className = "text-slate-500 pt-2">Post on 6th April 2003</div>
+							<div className = "pt-4">
+								{blog.content}
+							</div>
 						</div>
-						<div className = "">
-							{blog.content}
+						<div className = "col-span-4">
+						<div className = "text-slate-500 text-lg">Author</div>
+						<div className = "flex w-full">
+							<div className = "pr-4 flex flex-col justify-center">
+							<Avatar size="big" name = {blog.author.name || "Anonymous"}/>
+							</div>
+							<div>
+								<div className= "text-lg font-bold">{blog.author.name || "Ntihin Krishna"}</div>
+								<div className="pt-2 text-slate-500"> Random catch phrase about the author's ablity to grab user's attention</div>
+							</div>
 						</div>
-					</div>
-					<div className = "col-span-4">Hello
 					</div>
 				</div>
 			</div>
+		</div>
 }
